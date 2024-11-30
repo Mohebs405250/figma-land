494db2826cef25418e85ec02f9c93ccedf4dee60
moheb <134a137z@gmail.com>
2024-11-30 17:41:07 +0330

    finish mobile nav menu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let mobileNav = document.getElementById("mobileNav");
+let hamburger = document.getElementById("btn");
+hamburger.addEventListener("click",function(){
+    mobileNav.classList.toggle("left");
+
+})
